152134e351bcae12779f35423571b0a3be8f3153
wljess <62577566+huashanglhc@users.noreply.github.com>
2022-07-01 20:14:53 +0800

    feta:初始化项目目录

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：fastapiProject 
+@File    ：main.py
+@Author  ：WL
+@Date    ：2022/7/1 19:43 
+@Describe: 
+"""
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
